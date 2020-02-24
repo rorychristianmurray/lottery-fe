f72cd3dfb2a01e75b32bb32179150b5bd03c467a
Rory Murray <rory.christian.murray@gmail.com>
2020-02-23 21:11:56 -0500

    Enter contract supported

--- src/App.js
+++ src/App.js
@@ -7,8 +7,28 @@ class App extends React.Component {
   state = {
     manager: '',
     players: [],
-    balance: ''
+    balance: '',
+    value: ''
   }
+
+
+  onSubmit = async event => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts()
+
+    this.setState({message: 'Waiting on transaction success...'});
+
+    await lottery.methods.enter().send({
+        from: accounts[0],
+        value: web3.utils.toWei(this.state.value, 'ether')
+    });
+
+    this.setState({message: 'You have been entered'});
+
+  };
+
+
 
 
   async componentDidMount() {
@@ -28,6 +48,20 @@ class App extends React.Component {
             <h2>Lottery Contract</h2>
           <p>Contract managed by {this.state.manager}</p>
         <p>There are currently {this.state.players.length} players entered competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!</p>
+        <hr />
+        <form onSubmit={this.onSubmit}>
+          <h4>Try your luck</h4>
+          <div>
+            <label>Amount of ether to enter </label>
+            <input 
+            value={this.state.value}
+            onChange={event => this.setState({value: event.target.value  })}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
+        <hr />
+        <h2>{this.state.message}</h2>
           </div>
         );
     }
