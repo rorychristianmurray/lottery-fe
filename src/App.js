e5b570f8d8596ee30a06846136917f8a06145dac
Rory Murray <rory.christian.murray@gmail.com>
2020-02-23 20:09:25 -0500

    Call to manager working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import './App.css';
 import web3 from './web3'
+import lottery from "./lottery"
 
 class App extends React.Component {
-  render() {
-    
-console.log("web3.version : ", web3.version)
-web3.givenProvider.enable().then(console.log)
-
-
-    return (
-      <div className="App">
-        <div>App incoming</div>
-      </div>
-    );
+  state = {
+    manager: ''
   }
 
 
-}
+  async componentDidMount() {
+    console.log("CDM bef")
+    console.log("lottery.options.address : ", lottery.options.address)
+    const manager = await lottery.methods.manager().call()
+    this.setState({ manager })
+    console.log("CDM manager : ", manager)
+  }
+
+
+  render() {
+    console.log("state : ", this.state)
+    console.log("lottery : ", lottery)
+
+
+  console.log("web3.version : ", web3.version)
+    web3.givenProvider.enable().then(console.log)
+        return (
+          <div className="App">
+            <h2>Lottery Contract</h2>
+        <p>Contract managed by {this.state.manager}</p>
+          </div>
+        );
+    }
+
+  }
+
+
 
 export default App;
--- src/web3.js
+++ src/web3.js
@@ -1,6 +1,7 @@
 import Web3 from 'web3';
 
 // jack provider from injected metamask instance
-const w3 = new Web3(window.web3.currentProvider)
+const web3 = new Web3(window.web3.currentProvider)
 
-export default w3;
+export default web3;
+
