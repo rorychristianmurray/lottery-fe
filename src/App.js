b6b81d87df4cf2a570d0f3e8a0261f725eb1dd78
Rory Murray <rory.christian.murray@gmail.com>
2020-02-23 20:29:46 -0500

    Displaying more info

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ import lottery from "./lottery"
 
 class App extends React.Component {
   state = {
-    manager: ''
+    manager: '',
+    players: [],
+    balance: ''
   }
 
 
   async componentDidMount() {
-    const manager = await lottery.methods.manager().call()
-    this.setState({ manager })
+    const manager = await lottery.methods.manager().call();
+
+    const players = await lottery.methods.getPlayers().call()
+
+    const balance = await web3.eth.getBalance(lottery.options.address)
+
+
+    this.setState({ manager, players, balance })
   }
 
   render() {
         return (
           <div className="App">
             <h2>Lottery Contract</h2>
-        <p>Contract managed by {this.state.manager}</p>
+          <p>Contract managed by {this.state.manager}</p>
+        <p>There are currently {this.state.players.length} players entered competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!</p>
           </div>
         );
     }
